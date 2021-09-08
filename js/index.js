a1df501b3350f8fe2ccb0d0758158960f49e15f8
KatIsCoding <fabrigoru@gmail.com>
2021-09-08 15:19:14 -0600

    Linters corrections

--- js/index.js
+++ js/index.js
@@ -1,31 +1,24 @@
-
 // Change button's text when changing screen
 function searchID(id) {
-    return document.getElementById(id)
+  return document.getElementById(id);
 }
 
-
-const menu = searchID("mobile-menu")
-
+const menu = searchID('mobile-menu');
 
 function showMenu() {
-    menu.style.height = '100%';
+  menu.style.height = '100%';
 }
-
 
 function closeMenu() {
-    menu.style.height = '0';
+  menu.style.height = '0';
 }
-  
 
 window.onload = function asd() {
-    if (window.screen.width >= 768) {
-        const button = searchID("join-btn")
-        console.log(button)
-        button.innerText = "SEE ALL THE PROGRAM CONTENT HERE"
-    }
-    
-    
-    showMenu()
-    closeMenu()
-}
+  if (window.screen.width >= 768) {
+    const button = searchID('join-btn');
+    button.innerText = 'SEE ALL THE PROGRAM CONTENT HERE';
+  }
+
+  showMenu();
+  closeMenu();
+};
