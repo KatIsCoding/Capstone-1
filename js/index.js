cf05fa3ca84f626711627fe176f6a83cddea079d
KatIsCoding <fabrigoru@gmail.com>
2021-09-08 14:30:24 -0600

    Mobile Menu Functionality Added

--- js/index.js
+++ js/index.js
@@ -1,7 +1,22 @@
 
 // Change button's text when changing screen
+function searchID(id) {
+    return document.getElementById(id)
+}
 
 
+const menu = searchID("mobile-menu")
+
+
+function showMenu() {
+    menu.style.height = '100%';
+}
+
+
+function closeMenu() {
+    menu.style.height = '0';
+}
+  
 
 window.onload = function asd() {
     if (window.screen.width >= 768) {
